786c4efa2c8603c9fed24ac0987d0fca2cc712dd
Shakiba Sadat Vakili <122327036+shakiba-vakili@users.noreply.github.com>
2023-07-02 15:01:14 +0330

    week 2

---
+++ bulbs.c
@@ -0,0 +1,48 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+const int BITS_IN_BYTE = 8;
+
+void print_bulb(int bit);
+
+int main(void)
+{
+    // TODO
+    string message = get_string("Message: ");
+    // test: H
+
+
+    // conver to ascii
+    for (int i = 0, n = strlen(message); i < n; i++)
+    {
+        char c = message[i];
+        // text H = 72
+        // conver to binary
+        // H = 0 and 1
+        for (int j = BITS_IN_BYTE -1; j >= 0; j--)
+        {
+            // >> = shifting to right
+            int bit = (c >> j) & 1;
+
+        print_bulb(bit);
+        }
+
+    printf("\n");
+    }
+
+}
+
+void print_bulb(int bit)
+{
+    if (bit == 0)
+    {
+        // Dark emoji
+        printf("\U000026AB");
+    }
+    else if (bit == 1)
+    {
+        // Light emoji
+        printf("\U0001F7E1");
+    }
+}
